50461b14d7e2cf268bb242e2a041193361d95ed2
April <aprilnicole013@gmail.com>
2022-04-20 12:07:07 -0400

    Add forms module and ng model to html

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -15,4 +15,17 @@ export class AddTaskComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onSubmit(){
+    if(!this.text){
+      alert('Please add a task!')
+      return;
+    }
+
+    const newTask = {
+      text: this.text,
+      day: this.day,
+      reminder: this.reminder
+    }
+  }
+
 }
